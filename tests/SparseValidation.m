eb110833728531269e1b6973978c86d07f4557ab
TheoJJF <itstheo215@gmail.com>
2025-11-23 23:03:19 -0800

    Updated sparse test file

--- tests/SparseValidation.m
+++ tests/SparseValidation.m
@@ -2,7 +2,7 @@ classdef SparseValidation < matlab.unittest.TestCase
 
     methods (Test)
         
-        function case1(testCase)
+        function case1(testUpperTriCase)
             data = load(fullfile(pwd,"../data","SparseValidation.mat"));
             verification = load(fullfile(pwd,"../data","expected.mat"));
 
@@ -11,11 +11,38 @@ classdef SparseValidation < matlab.unittest.TestCase
             expected = verification.expected;
 
             actual = SGEPP(A,b);
-            testCase.verifyEqual(actual,expected);
+            testUpperTriCase.verifyEqual(actual,expected);
         end
 
-        
+        function case2(testSolutionCase)
+            data = load(fullfile(pwd,"../data","SparseValidation.mat"));
+            
+            sparse_A = data.A;
+            sparse_b = data.b;
 
+            A = [2,-1,0,0;-1,2,-1,0;0,-1,2,-1;0,0,-1,2];
+            b = ones(4,1);
+
+            actual = SolveSGEPP(sparse_A,sparse_b);
+            expected = A\b;
+
+            testSolutionCase.verifyEqual(actual,expected);
+        end
+
+        function case3(testSolutionCase)
+            data = load(fullfile(pwd,"../data","SparseValidation.mat"));
+            
+            sparse_A = data.A;
+            sparse_b = data.b;
+
+            A = [2,-1,0,0;-1,2,-1,0;0,-1,2,-1;0,0,-1,2];
+            b = ones(4,1);
+
+            actual = SolveSGEPP(sparse_A,sparse_b);
+            expected = SolveGEPP(A,b);
+
+            testSolutionCase.verifyEqual(actual,expected);
+        end
     end
 
 end
